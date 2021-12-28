f94d751a369a8522075318626e2c077c4969c11d
xgen-subhash <subhash@xgengroup.com.au>
2021-12-28 20:02:02 +0530

    first commit

---
+++ src/main/java/com/smartops/extenstion/settings/Settings.kt
@@ -0,0 +1,36 @@
+package com.smartops.extenstion.settings
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.smartops.extenstion.settings.ui.main.SectionsPagerAdapter
+import com.smartops.extenstion.settings.databinding.ActivitySettingsBinding
+
+class Settings : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = binding.viewPager
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = binding.tabs
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = binding.fab
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
